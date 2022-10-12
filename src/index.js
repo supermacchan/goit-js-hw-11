bfa9980fecad0241bb68e27df03f3dd8bc8ed94d
Olga Birkovska <birkovskaya92@ukr.net>
2022-10-12 20:51:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import { fetchImages } from "./fetch-images";
 
+const form = document.querySelector('.search-form');
+const formInput = document.querySelector('input');
+
+
+form.addEventListener('submit', onSearchSubmit);
+
+function onSearchSubmit(e) {
+    e.preventDefault();
+}
+
 fetchImages('cat');
+
+const IMAGE_CARD_MARKUP = `<div class="photo-card">
+    <img src="" alt="" loading="lazy" />
+    <div class="info">
+        <p class="info-item">
+            <b>Likes</b>
+        </p>
+        <p class="info-item">
+            <b>Views</b>
+        </p>
+        <p class="info-item">
+            <b>Comments</b>
+        </p>
+        <p class="info-item">
+            <b>Downloads</b>
+        </p>
+    </div>
+</div>`;
