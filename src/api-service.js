7af55169bf23c8c1e4c9161385ca9b88a3f0dffc
Olga Birkovska <birkovskaya92@ukr.net>
2022-10-14 21:19:40 +0300

    Update api-service.js

--- src/api-service.js
+++ src/api-service.js
@@ -1,9 +1,8 @@
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '30551653-aa9d35c8f88064a7bc9ad69bf';
 let pageNumber = 1;
 
 async function fetchImages(query) {
-    const BASE_URL = 'https://pixabay.com/api/';
-    const API_KEY = '30551653-aa9d35c8f88064a7bc9ad69bf';
-
     const response = await fetch(`${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${pageNumber}`);
     const fetchedImages = await response.json();
     pageNumber += 1;
